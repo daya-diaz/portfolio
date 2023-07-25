be7fe7b610bfbd3293ab7405e15fc3a608799863
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2023-07-25 14:31:32 -0300

    colocando pdf do cv.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Home.css';
 import Avatar from '../../Assets/avatar.png'
-import Arrow from '../../Assets/arrow-right.svg'
+import Arrow from '../../Assets/arrow-right.svg';
+import CV from '../../CV.pdf';
 const Home = () => {
   return (
     <div className='container home'>
@@ -13,8 +14,8 @@ const Home = () => {
         <h1>Desenvolvo <span className='highlighted-text'>ideias</span> e ajudo a construir um mundo melhor através do software.</h1>
       </div>
       <div className="home-buttons-content">
-        <a href="#">Contato</a>
-        <button className='downloadCV'>Download CV <img src={Arrow} alt="" /></button>
+        <a target='_blank' href="https://wa.me/558198595825" rel="noreferrer">Contato</a>
+        <a href={CV} download='Dayane_Santos_Dev_CV.pdf' className='downloadCV'>Download CV <img src={Arrow} alt="" /></a>
       </div>
     </div>
   )
