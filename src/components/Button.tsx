4296ac5bdcef98ae730b586227dc6ea3b89e4090
Dayane Santos <dayadiazprofissional@gmail.com>
2024-04-30 14:19:57 -0300

    adicionando efeito de hover no Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ export function Button({
     <a
       target="_blank"
       href={href}
-      className={` border-white border-2 px-4 py-2 rounded-lg font-medium font-poppins text-sm ${type === 'primary' ? 'bg-white text-black' : 'bg-black text-white'} ${size === 'big' ? 'bg-black px-12 py-4 border-2 border-gray-200 rounded-full text-base' : ''}`}
+      className={` border-white border-2 px-4 py-2 rounded-lg font-medium font-poppins text-sm transition hover:bg-blue-500 hover:text-white hover:border-blue-500 ${type === 'primary' ? 'bg-white text-black ' : 'bg-black text-white'} ${size === 'big' ? 'bg-black px-12 py-4 border-2 border-gray-200 rounded-full text-base' : ''}`}
       rel="noreferrer"
     >
       {textButton}
